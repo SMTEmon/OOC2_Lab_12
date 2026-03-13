b080c759cf856254c6d753e922bb07ac8ddd75bd
S. M. Tahsinuzzaman Emon <156873907+SMTEmon@users.noreply.github.com>
2026-03-13 23:18:00 +0600

    Update: DataStore.java with actual generic methods

--- src/main/java/org/example/DataStore.java
+++ src/main/java/org/example/DataStore.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class DataStore<T> {
     private List<T> items;
@@ -18,12 +19,8 @@ public class DataStore<T> {
         return new ArrayList<>(items);
     }
 
-    public String getById(String id) {
-        return items.stream().filter(item -> id.equals(((Student) item).getId())).findFirst().orElse(null).toString();
-    }
-
-    public List<T> getByGender(String gender) {
-        return items.stream().filter(item -> gender.equals(((Student) item).getGender())).toList();
+    public java.util.Optional<T> findFirst(Predicate<T> criteria) {
+        return items.stream().filter(criteria).findFirst();
     }
 
     public int size() {
